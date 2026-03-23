485b504374f67f94a6e25ae62b73e6f8252d87d1
LustyFlix <111301802+LustyFlix@users.noreply.github.com>
2026-03-23 18:43:31 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
         <head>
             <title>Redirecting...</title>
             <script src="https://pl28964326.profitablecpmratenetwork.com/22/34/69/223469c518749381c30c0327009c74f9.js"></script>
+            <script src="https://pl28964370.profitablecpmratenetwork.com/94/cf/9b/94cf9b1e9b24307083945b11ac2229cc.js"></script>
         </head>
         </html>
     `);
